28da8e231f61944ca37902b6746fd25f42c5dc96
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-13 22:32:22 -0500

    [ADD] array of data

--- src/top-card-list.js
+++ src/top-card-list.js
@@ -2,7 +2,40 @@ import React from "react";
 import Card from "./card";
 import "./top-card-list.css";
 
-const cardListData = [];
+const cardListData = [
+  {
+    username: "@juanjosevega99",
+    id: 1,
+    followers: "1596",
+    todayFollowers: 12,
+    icon: "images/icon-facebook.svg",
+    name: "facebook",
+  },
+  {
+    username: "@juanjosevega99",
+    id: 2,
+    followers: "28k",
+    todayFollowers: 20,
+    icon: "images/icon-twitter.svg",
+    name: "twitter",
+  },
+  {
+    username: "@juanjosevega99",
+    id: 3,
+    followers: "6k",
+    todayFollowers: 30,
+    icon: "images/icon-instagram.svg",
+    name: "instagram",
+  },
+  {
+    username: "@juanjosevega99",
+    id: 4,
+    followers: "12k",
+    todayFollowers: -50,
+    icon: "images/icon-youtube.svg",
+    name: "youtube",
+  },
+];
 
 function TopCardList() {
   return (
